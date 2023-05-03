1e0ef0b8dba48cb204f918535a0e3e365547ca62
Tuure Kaunisto <tuure.kaunisto@gmail.com>
2023-05-04 00:30:25 +0300

    Pre-process the json exports so we only need to download the needed fields

---
+++ assets/extract-post-data.mjs
@@ -0,0 +1,27 @@
+import fs from 'fs'
+import posts0 from './old-posts-0.json' assert { type: "json" }
+import posts1 from './old-posts-1.json' assert { type: "json" }
+
+function extractFields(post) {
+    return {
+        ID: post.ID,
+        date: post.date,
+        title: post.title,
+        content: post.content
+            .replace(/data-[^"\s]+"[^"]*"(\s)?/ig, '')
+            .replace(/<div[^<>]*style="float:left;width:100%;height:\d*px;overflow:hidden;"[^<>]*>/ig, '<div class="deleted">')
+            .replace(/style="[^"]*"/ig, '')
+            .replace(/href=["']https?:\/\/clubinfektio\.com[^"']*["']/ig, '')
+            .replace(/href=["']https?:\/\/infektio\.(files\.)?wordpress\.com[^"']*["']/ig, '')
+            .replace(/href=["']https?:\/\/infektio\.wordpress\.com[^"']*["']/ig, '')
+    }
+}
+
+function minifyPosts(postData) {
+    return {
+        posts: postData.posts.map(extractFields)
+    }
+}
+
+fs.writeFileSync('./old-posts-0-minimal.json', JSON.stringify(minifyPosts(posts0)), 'utf8')
+fs.writeFileSync('./old-posts-1-minimal.json', JSON.stringify(minifyPosts(posts1)), 'utf8')
